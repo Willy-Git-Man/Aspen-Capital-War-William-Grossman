a7f522374c801263438088aa2e696f510d59ff57
Willy-Git-Man <83353737+Willy-Git-Man@users.noreply.github.com>
2022-07-12 17:11:36 -0400

    logic almost finished

--- frontend/src/components/GameLogic/index.js
+++ frontend/src/components/GameLogic/index.js
@@ -5,17 +5,9 @@ function GameLogic() {
   const [playing, setPlaying] = useState(false)
   const [homeDeckState, setHomeDeckState] = useState([])
   const [opponentDeckState, setOpponentDeckState] = useState([])
-  const [homeWinngPile, setHomeWinningPile] = useState([])
-  const [opponentWinningPile, setOpponentWinningPile] = useState([])
   const [currentHomeCard, setCurrentHomeCard] = useState()
   const [currentOpponentCard, setCurrentOpponentCard] = useState()
-
-
-
   const [message, setMessage] = useState("Ready")
-
-
-  console.log(playing)
 
   const newGame = () => {
     setPlaying(false)
@@ -23,11 +15,10 @@ function GameLogic() {
     setOpponentDeckState([])
     setCurrentHomeCard()
     setCurrentOpponentCard()
-    setHomeWinningPile([])
-    setOpponentWinningPile([])
   }
 
   const initialShuffle = () => {
+    setMessage('Shuffled')
     const cardSuit = ["♤", "♧", "♡", "♢"]
     const cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
     for (let i = 0; i < 4; i++) {
@@ -66,66 +57,30 @@ function GameLogic() {
 
   }
 
-  const shuffleHome = (deck) => {
+  const nextHand =  () => {
+    setMessage("Playing")
 
-    console.log(opponentDeckState)
-
-    for (let i = 0; i < deck.length; i++) {
-      if (i % 2 === 0) {
-        let temp = deck[i]
-        deck[i] = deck[i + 1]
-        deck[i + 1] = temp
-      }
-
-    }
-    setHomeWinningPile(deck)
-  }
-
-  const shuffleOpponent = () => {
-    const deck = []
-
-    for (let i = 0; i < opponentDeckState.length; i++) {
-      if (i % 2 === 0) {
-        deck.push(opponentDeckState[i])
-      } else deck.unshift(opponentDeckState[i])
-    }
-    setOpponentWinningPile(deck)
-  }
-
-  const nextHand = () => {
-    if (opponentDeckState.length === 0 && opponentWinningPile.length === 0) console.log('winning')
-    if (homeDeckState.length === 0) {
-      setHomeDeckState(homeWinngPile)
-      setHomeWinningPile([])
-      return
-    }
-
-    if (opponentDeckState.length === 0) {
-      setOpponentDeckState(opponentWinningPile.reverse())
-      setOpponentWinningPile([])
-      return
-    }
-
-    const homeCard = homeDeckState[0]
-    const opponentCard = opponentDeckState[0]
-
+    const homeCard = homeDeckState.pop()
+    const opponentCard = opponentDeckState.pop()
     setCurrentHomeCard(homeCard)
     setCurrentOpponentCard(opponentCard)
 
-    setHomeDeckState(homeDeckState.splice(1, homeDeckState.length - 1))
-    setOpponentDeckState(opponentDeckState.splice(1, opponentDeckState.length - 1))
 
 
-    if (homeCard[0] > opponentCard[0]) {
-      setHomeWinningPile(homeWinngPile.concat([homeCard, opponentCard]))
-    } else if (opponentCard[0] > homeCard[0]) {
-      setOpponentWinningPile(opponentWinningPile.concat([homeCard, opponentCard]))
-    } else if (homeCard[0] === opponentCard[0]) {
-      setHomeWinningPile(homeWinngPile.concat([homeCard, opponentCard]))
+    if (homeCard[0] > opponentCard[0]) homeDeckState.push(homeCard,opponentCard)
+    else if (homeCard[0] < opponentCard[0]) opponentDeckState.push(homeCard,opponentCard)
+    else {
+      const homeDown = homeDeckState.pop()
+      const homeUp = homeDeckState.pop()
+      const opponentDown = opponentDeckState.pop()
+      const opponentUp = opponentDeckState.pop()
 
+      if (homeUp[0] >= opponentUp[0]) homeDeckState.push(homeCard,opponentCard, homeDown,homeUp,opponentDown,opponentUp)
+      else if (homeUp[0] < opponentUp[0]) homeDeckState.push(homeCard,opponentCard, homeDown,homeUp,opponentDown,opponentUp)
     }
 
-  }
+
+    }
 
   return (
     <>
@@ -138,26 +93,27 @@ function GameLogic() {
             <button onClick={() => nextHand()}>Next Hand</button>
           </>
 
-
         )}
       </div>
 
       <div className="playingGameDiv">
         <div className="homeSide">
           <p>Home Deck : {homeDeckState.length}</p>
-          <p>Winning Pile: {homeWinngPile.length}</p>
+          {/* <p>Winning Pile: {homeWinngPile.length}</p> */}
           <h1>{currentHomeCard}</h1>
         </div>
 
         <div className="opponentSide">
           <p>Opponent : {opponentDeckState.length}</p>
-          <p>Winning Pile : {opponentWinningPile.length}</p>
+          {/* <p>Winning Pile : {opponentWinningPile.length}</p> */}
           <h1>{currentOpponentCard}</h1>
 
         </div>
 
-
       </div>
+          <div>
+            {message}
+          </div>
     </>
   )
 }
